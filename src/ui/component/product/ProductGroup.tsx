d4938baffc11459a41ba82c43a81f75387d86714
BcnChsBrgr <yuhinng41@gmail.com>
2024-04-18 22:48:55 +0100

    add a tag for going to product details page

--- src/ui/component/product/ProductGroup.tsx
+++ src/ui/component/product/ProductGroup.tsx
@@ -12,35 +12,37 @@ export default function ProductGroup({ productGroup }: Props) {
     const [product, setProduct] = useState(productGroup.at(0));
     console.log(product);
     return (
-        <Card variant="outlined" sx={{ maxWidth: 345 }}>
-            <CardMedia
-                sx={{ height: 180 }}
-                image={product.image_url}
-                title={product.name}
-            />
-            <CardContent>
-                {productGroup.map((productItem, index) => {
-                    return (
-                        <span
-                            key={index}
-                            style={{
-                                height: 25,
-                                width: 25,
-                                display: "inline-block",
-                                borderRadius: "50%",
-                                backgroundColor: productItem.colour,
-                            }}
-                            onMouseOver={() => setProduct(productItem)}
-                        ></span>
-                    );
-                })}
-                <Typography gutterBottom variant="h6" component="div">
-                    {product.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    $ {product.price}
-                </Typography>
-            </CardContent>
-        </Card>
+        <a href={`/product/${product.pid}/99`}>
+            <Card variant="outlined" sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 180 }}
+                    image={product.image_url}
+                    title={product.name}
+                />
+                <CardContent>
+                    {productGroup.map((productItem, index) => {
+                        return (
+                            <span
+                                key={index}
+                                style={{
+                                    height: 25,
+                                    width: 25,
+                                    display: "inline-block",
+                                    borderRadius: "50%",
+                                    backgroundColor: productItem.colour,
+                                }}
+                                onMouseOver={() => setProduct(productItem)}
+                            ></span>
+                        );
+                    })}
+                    <Typography gutterBottom variant="h6" component="div">
+                        {product.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        $ {product.price}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </a>
     );
 }
